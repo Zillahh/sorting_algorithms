3cd7be48387665055d3150da245d42c97c522b22
Zillahh <ugwubonifaceotu1998@gmail.com>
2024-04-30 16:51:24 -0700

    sorting algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,15 +22,15 @@ void bubble_sort(int *array, size_t size)
 		return;
 	}
 
-	for (i =0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (o = 0; o < size -1; o++)
+		for (o = 0; o < size - 1; o++)
 		{
 			if (array[o] > array[o + 1])
 			{
 				tmp = array[o];
 				array[o] = array[o + 1];
-				array[o + 1] =tmp;
+				array[o + 1] = tmp;
 				print_array(array, size);
 				check += 1;
 			}
